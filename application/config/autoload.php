cf13de5298eb57d14066961ec0975dd65358356c
Juan Carlos Peña <pegnacorp@gmail.com>
2015-05-23 01:21:29 -0500

    Integrando bootstrap

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -60,7 +60,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
 
-$autoload['libraries'] = array();
+$autoload['libraries'] = array('database');
 
 
 /*
@@ -82,13 +82,13 @@ $autoload['drivers'] = array();
 /*
 | -------------------------------------------------------------------
 |  Auto-load Helper Files
-| -------------------------------------------------------------------
+| -------------------------------------------------------------------	
 | Prototype:
 |
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array();
+$autoload['helper'] = array('url','date');
 
 
 /*
