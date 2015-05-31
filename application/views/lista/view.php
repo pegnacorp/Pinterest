b0fddf08bec69ac4dd71cc2f59a8b609e8f5a8d1
Arian Castillo <caztle501@gmail.com>
2015-05-31 00:52:33 -0500

    Crear enlaces

---
+++ application/views/lista/view.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo $lista->Nombre ?> - Pinterestellar</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+</head>
+<body>
+<h1 align="center"><?php echo $lista->Nombre ?></h1>
+<div class="container" align="center">
+
+</div>
+</body>
+</html>
