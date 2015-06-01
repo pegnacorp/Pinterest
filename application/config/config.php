d6161ccc98814bb10c930c4e034ceaaa56c5a298
Jahzeel Coss <gear_jahzeel24@hotmail.com>
2015-06-01 07:59:25 -0500

    autentificacion y crud de usuarios

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url']    = 'http://localhost/Codeigniter/';
+$config['base_url']    = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -71,7 +71,7 @@ $config['url_suffix'] = '';
 | than english.
 |
 */
-$config['language']	= 'english';
+$config['language']	= 'spanish';
 
 /*
 |--------------------------------------------------------------------------
