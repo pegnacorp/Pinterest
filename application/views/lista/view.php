40f9b3f1c563e15671c86986ddee2216dde8da3c
Arian Castillo <caztle501@gmail.com>
2015-06-02 13:55:15 -0500

    Último cambio visual a Listas

--- application/views/lista/view.php
+++ application/views/lista/view.php
@@ -1,5 +1,8 @@
 <br>
-<h1 align="center"><?php echo $lista->Nombre ?></h1>
+<h1 align="center">
+	<?php echo $lista->Nombre ?><br>
+	<a href="<?php echo base_url();?>index.php/lista" class="btn btn-danger"><span class="glyphicon glyphicon-chevron-left"></span> Regresar</a>
+</h1>
 <div class="container" align="center">
 <br><br>
 </div>
